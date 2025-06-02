9100331cb91fb3fff45cdfca4b166bfda57dad33
Miki <migwaraa@gmail.com>
2025-06-02 08:57:58 +0700

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -13,7 +13,7 @@ const socialLinks = [
     displayName: "Let's Connect",
     subText: "on LinkedIn",
     icon: Linkedin,
-    url: "https://www.linkedin.com/in/migwara/",
+    url: "https://www.linkedin.com/in/MuhammadRifkiApreliant/",
     color: "#0A66C2",
     gradient: "from-[#0A66C2] to-[#0077B5]",
     isPrimary: true
